d58d5f8f39874575c8a6e7e45dc3515b9a4d6a8b
purfeld <wojo111115@wp.pl>
2020-08-04 19:16:35 +0200

    Koniec podstaw

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace Learning
 {
@@ -6,13 +7,73 @@ namespace Learning
     {
         static void Main(string[] args)
         {
-            //string name = args[0];
-            //Console.WriteLine("Hello World! " + name);
+            ValueTypes();
 
-            foreach (var item in args)
+            for (; ;)
             {
-                Console.WriteLine("Witaj, " + item);
+                Greeting();
+                Calculation();
+                Settings();
+
+                //foreach (var item in args)
+                //{
+                //    Console.WriteLine("Witaj, " + item);
+                //}
             }
+        }
+
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+            Console.WriteLine("Max INT = " + maxInt);
+            Console.WriteLine("Min INT = " + minInt);
+            Console.WriteLine("Max LONG = " + maxLong);
+            Console.WriteLine("Min LONG = " + minLong);
+        }
+
+        /// <summary>
+        /// Ustawienia
+        /// </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+        /// <summary>
+        /// Komunikat zależny od wieku
+        /// </summary>
+        private static void Calculation()
+        {
+            Console.WriteLine("Wpisz ile masz lat: ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Brawo. Jesteś pełnoletni");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Zły wiek");
+            }
+            else
+            {
+                Console.WriteLine("Puedes beber la leche");
+            }
+        }
+        /// <summary>
+        /// Wypisujemy powitanie użytkownika
+        /// </summary>
+        private static void Greeting()
+        {
+            Console.Write("Podaj imie: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Witaj " + name);
         }
     }
 }
